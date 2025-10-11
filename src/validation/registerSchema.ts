a92b7d2eea8cc53a82d318627c369a65aa998975
Sah-Rahul <sahfamilyinfo@gmail.com>
2025-10-11 15:04:07 +0545

    checking git

--- src/validation/registerSchema.ts
+++ src/validation/registerSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const registerSchema = z
   .object({
-    name: z.string().min(2, "Name must be at least 2 characters"),
+    name: z.string().min(2, "Name must be at least 5 characters"),
     userName: z.string().min(3, "Username must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
     role: z.enum(["applicant", "recruiter"]),
